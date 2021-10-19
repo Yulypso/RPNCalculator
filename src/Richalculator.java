d6d0cf7d28c756b0a19e5d24af6996dd929287af
Yulypso <khamphousone.thierry@outlook.fr>
2021-10-20 00:46:48 +0200

    + Parser Number and Vector

--- src/Richalculator.java
+++ src/Richalculator.java
@@ -1,24 +1,114 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class Richalculator {
 
+    private static String typeDetector(String token) {
+        Pattern addRegex = Pattern.compile("^([+])");
+        Pattern subRegex = Pattern.compile("^([\\-])");
+        Pattern mulRegex = Pattern.compile("^([*])");
+        Pattern divRegex = Pattern.compile("^([/])");
+        Pattern modRegex = Pattern.compile("^([%])");
+
+        Pattern numberRegex = Pattern.compile("^(([-+])?[0-9])*([.]([0-9]+))?$");
+        Pattern vectorRegex = Pattern.compile("^(([\\[])(([-+])?[0-9])*([.]([0-9]+))?)((,)(([-+])?[0-9])*([.]([0-9]+))*)*([]])$");
+
+        System.out.println(token);
+
+        if (token.equals("clear") || token.equals("c")) {
+            System.out.println("CLEAR");
+            return "CLEAR";
+        }
+        else if (token.equals("quit") || token.equals("q")) {
+            System.out.println("QUIT");
+            return "QUIT";
+        }
+        else if (addRegex.matcher(token).matches()) {
+            System.out.println("ADD MATCH");
+            return "ADD";
+        }
+        else if (subRegex.matcher(token).matches()) {
+            System.out.println("SUB MATCH");
+            return "SUB";
+        }
+        else if (mulRegex.matcher(token).matches()) {
+            System.out.println("MUL MATCH");
+            return "MUL";
+        }
+        else if (divRegex.matcher(token).matches()) {
+            System.out.println("DIV MATCH");
+            return "DIV";
+        }
+        else if (modRegex.matcher(token).matches()) {
+            System.out.println("MOD MATCH");
+            return "MOD";
+        }
+
+        else if (vectorRegex.matcher(token).matches()) {
+            return "VECTOR";
+        }
+        else if (numberRegex.matcher(token).matches()) {
+            return "NUMBER";
+        }
+
+        return null;
+    }
+
     public static void main(String[] args) {
-        PileRPL pile = new PileRPL(4);
 
-        //ObjEmp oe1 = new Vecteur(new Double[]{1.0, 2.0, 3.0});
-        //ObjEmp oe2 = new Vecteur(new Double[]{4.0, 5.0, 4.0});
+        PileRPL pile = new PileRPL();
 
-        ObjEmp oe1 = new Nombre(4.0);
-        ObjEmp oe2 = new Nombre(1.0);
+        Scanner scanner = new Scanner(System.in);
+        String token;
 
-        //ObjEmp oe1 = new Complexe(1.0, 2.0);
-        //ObjEmp oe2 = new Complexe(0.0, 1.0);
-        //ObjEmp oe2 = new Complexe(2.0);
+        loop: while (true) {
+            token = scanner.nextLine().trim();
+            System.out.println("token: " + token);
 
-        System.out.println(pile);
-        pile.stack(oe1);
-        System.out.println(pile);
-        pile.stack(oe2);
-        System.out.println(pile);
-        pile.mod();
-        System.out.println(pile);
+            try {
+                switch (typeDetector(token)) {
+                    case "QUIT" -> {
+                        break loop;
+                    }
+                    case "CLEAR" -> pile.clear();
+                    case "NUMBER" -> pile.stack(new Nombre(Double.parseDouble(token)));
+                    case "COMPLEX" -> {
+
+                    }
+                    case "VECTOR" -> {
+                        System.out.println("PASS");
+                        String[] stringArray = token.trim().replaceAll("\\[", "").replaceAll("]", "").split(",");
+                        Double[] doubleArray = Arrays.stream(stringArray)
+                                .map(Double::valueOf)
+                                .toArray(Double[]::new);
+                        pile.stack(new Vecteur(doubleArray));
+                    }
+                    case "ADD" -> {
+                        if (pile.pile.size() > 1) pile.add();
+                        else System.err.println("[Warning] - Not enough operands.");
+                    }
+                    case "SUB" -> {
+                        if (pile.pile.size() > 1) pile.sub();
+                        else System.err.println("[Warning] - Not enough operands.");
+                    }
+                    case "MUL" -> {
+                        if (pile.pile.size() > 1) pile.mul();
+                        else System.err.println("[Warning] - Not enough operands.");
+                    }
+                    case "DIV" -> {
+                        if (pile.pile.size() > 1) pile.div();
+                        else System.err.println("[Warning] - Not enough operands.");
+                    }
+                    case "MOD" -> {
+                        if (pile.pile.size() > 1) pile.mod();
+                        else System.err.println("[Warning] - Not enough operands.");
+                    }
+                    case null, default -> System.err.println("[Warning] - Unknown input: " + token);
+                }
+            } catch (Exception e) {System.err.println("[Warning] - " + e.getMessage());}
+            System.out.println(pile);
+        }
+        scanner.close();
     }
 }
