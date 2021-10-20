bf3837d29efb76a94958a6a3aad188b5a4c33451
Yulypso <khamphousone.thierry@outlook.fr>
2021-10-20 23:59:51 +0200

    update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/local/bin/zsh
 
-javac --enable-preview --release 17 src/*.java -d out
+javac --enable-preview --release 17 src/*.java src/Models/*.java src/Utils/*.java -d out
 java --enable-preview -cp out Richalculator
